8741e01e5feb6ef8ee49886493b273dfed6df027
Efraim Munthe <efrayanglain@gmail.com>
2024-02-25 18:17:06 +0700

    connect to wifi and send message via /start and /status

--- src/main.cpp
+++ src/main.cpp
@@ -10,40 +10,72 @@ WiFiClientSecure client;
 UniversalTelegramBot bot(BOT_TOKEN, client);
 
 #define MOISTURE_SENSOR_PIN A0
-int moistureValue;
 
-int botRequestDelay = 1000;
-unsigned long lastTimeBotRan;
-
-int getMoistureValue();
-
-void setup() {
-  Serial.begin(115200);
-}
-
-void loop() {
-  moistureValue = getMoistureValue();
-  Serial.println("Moisture value: ");
-  Serial.println(moistureValue);
+String getMoistureValue() {
+  int moistureValue = analogRead(MOISTURE_SENSOR_PIN);
+  String message;
 
   if (moistureValue < 500) {
-    Serial.println("tanaman lu kering anjing");
+    message = "tanaman lu kering anjing";
   } else if (moistureValue > 500 && moistureValue < 700) {
-    Serial.println("udah bagus sih");
+    message = "udah bagus sih";
   } else {
-    Serial.println("terlalu basah coy");
+    message = "terlalu basah coy";
   }
 
-  // so we don't get output every millisecond
-  delay(5000);
-}
-
-int getMoistureValue() {
-  int val = analogRead(MOISTURE_SENSOR_PIN);
-  return val;
+  return String(moistureValue) + ": " + message;
 }
 
 void handleNewMessages(int newNumMessages) {
-  Serial.println("handling message");
-  Serial.println(String(newNumMessages));
+  for (int i = 0; i < newNumMessages; i++) {
+    String incomingChatID = String(bot.messages[i].chat_id);
+
+    if (incomingChatID != CHAT_ID) {
+      bot.sendMessage(CHAT_ID, "WHO THE HELL ARE YOU");
+      continue;
+    }
+
+    String incomingText = String(bot.messages[i].text);
+
+    if (incomingText == "/start") {
+      String welcome = "Halo"  + bot.messages[i].from_name + " anjing...";
+      bot.sendMessage(CHAT_ID, welcome, "");
+    } else if (incomingText == "/status") {
+      String moistureValue = getMoistureValue();
+      bot.sendMessage(CHAT_ID, moistureValue, "");
+    }  else {
+      bot.sendMessage(CHAT_ID, incomingText, "");
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  configTime(0, 0, "pool.ntp.org");
+  client.setTrustAnchors(&cert);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("connecting to WiFi..");
+  }
+
+  Serial.println("connected to WiFi");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() { 
+  delay(100);
+
+  int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+
+  while(numNewMessages) {
+    Serial.println("got response");
+    handleNewMessages(numNewMessages);
+    numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+  }
+
+  Serial.println("scanning for messages...");
 }
